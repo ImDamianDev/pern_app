76d0e4bf4909df5dae1f24c29c47e23c6f15b89b
ImDamian <dcortesmunoz@gmail.com>
2022-12-07 00:47:23 -0300

    proyecto crado correctamente

---
+++ index.js
@@ -0,0 +1,33 @@
+//import dependencies
+const express = require('express');
+
+//initialization
+const app = express();
+
+//settings
+const HOST = 'localhost';
+const PORT = 3000
+
+//routes
+app.get("/", ( req, res ) => {
+    res.send("Hola Mundo, recordando como crear un servidor con express")
+});
+
+app.get("/QuienSoy",( req, res ) => {
+    res.send("Hi Im Damian")
+});
+
+app.get("/Home/:id", ( req, res ) => {
+    res.send(req.params.id)
+});
+
+app.get("*", ( req, res ) => {
+    res.send(`<p style="color:red">La dirección que estás consultando no
+    existe </p>`)
+});
+
+
+//starting the server
+app.listen(PORT, ()=>{
+    console.log(`\n=========================================\nserver is running\n\nhost: ${HOST}\nport: ${PORT}\nhttp://${HOST}:${PORT}/\n=========================================`)
+})
